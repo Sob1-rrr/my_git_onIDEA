127d670445f1d7b76005f6c664ea7939ea9347a3
Sober <yangSober@163.com>
2022-09-27 20:47:07 +0800

    my change test

--- src/main/java/com/itheima/SpringBoot01CreateProjectApplication.java
+++ src/main/java/com/itheima/SpringBoot01CreateProjectApplication.java
@@ -8,6 +8,9 @@ public class SpringBoot01CreateProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot01CreateProjectApplication.class, args);
+
+
+
     }
 
 }
